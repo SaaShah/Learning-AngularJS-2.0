8b97e966f561b9162a2f8be8762499487e2e8e35
saadbinshahid <saadbinshahid.fb@gmail.com>
2016-02-12 01:23:35 +0500

    adding app.

---
+++ src/app/app.component.js
@@ -0,0 +1,14 @@
+/**
+ * Created by mrx on 2/12/16.
+ */
+
+(function(app) {
+    app.AppComponent =
+        ng.core.Component({
+            selector: 'my-app',
+            template: '<h1>My First Angular 2 App</h1>'
+        })
+            .Class({
+                constructor: function() {}
+            });
+})(window.app || (window.app = {}));
---
+++ src/app/main.js
@@ -0,0 +1,9 @@
+/**
+ * Created by mrx on 2/12/16.
+ */
+
+(function(app) {
+    document.addEventListener('DOMContentLoaded', function() {
+        ng.platform.browser.bootstrap(app.AppComponent);
+    });
+})(window.app || (window.app = {}));

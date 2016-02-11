9d46a8f6b8f32e89986b19f012dddbf967af1385
saadbinshahid <saadbinshahid.fb@gmail.com>
2016-02-12 01:36:41 +0500

    adding app-2.

--- src/app/app.component.js
+++ src/app/app.component.js
@@ -12,3 +12,16 @@
                 constructor: function() {}
             });
 })(window.app || (window.app = {}));
+
+(function(app) {
+    app.AppComponent2 =
+        ng.core.Component({
+            selector: 'my-app-2',
+            template: '<h1>My Second Angular 2 App</h1>'
+        })
+            .Class({
+                constructor: function() {
+                    console.log('constructor() @ my-app-2');
+                }
+            });
+})(window.app || (window.app = {}));
